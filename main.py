8c8874bd15c73d7614864f88012870b50b3ada66
Maxim-Pekov <max.pekov@gmail.com>
2022-07-26 12:54:21 +0300

    add promoted image, where price is min

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ import pandas
 from pprint import pprint
 import collections
 
-excel_wines = pandas.read_excel('wine3.xlsx', na_values='some_dummy_na_value', keep_default_na=False)
-wines = excel_wines.to_dict(orient='records')
-wines_dict = collections.defaultdict(list)
-for i in wines:
-    wines_dict[i['Категория']].append(i)
-    # if i['Категория'] in slovar.keys():
-    #     slovar[i['Категория']].append(i)
-    # else:
-    #     slovar[i['Категория']] = [i]
-pprint(len(wines_dict.keys()))
-pprint(wines)
-for i in wines_dict:
-    print(i)
 
+def get_excel_wines():
+    '''Возвращает словарь с данными о вине полученным из Excel файла клиента'''
+
+    excel_wines = pandas.read_excel('wine3.xlsx', na_values='some_dummy_na_value', keep_default_na=False)
+    wines = excel_wines.to_dict(orient='records')
+    wines_dict = collections.defaultdict(list)
+    for i in wines:
+        wines_dict[i['Категория']].append(i)
+    # pprint(wines_dict)
+    return wines_dict
 
 
 def days_with_you():
@@ -27,9 +24,9 @@ def days_with_you():
     date_start = datetime.datetime(year=1920, month=1, day=1, hour=0)
     today = datetime.datetime.now()
     years = today.year - date_start.year
-    if years % 100 in [11,12,13,14]:
+    if years % 100 in [11, 12, 13, 14]:  # функция возвращает слово год в правильной форме в зависимости от окончания
         return f'{years} лет'
-    if years % 10 == 1:             # функция возвращает слово год в правильной форме в зависимости от окончания
+    if years % 10 == 1:
         return f'{years} год'
     elif 0 < years % 10 < 5:
         return f'{years} года'
@@ -43,7 +40,7 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-rendered_page = template.render(days_with_you=days_with_you(), wines=wines, wines_dict=wines_dict)
+rendered_page = template.render(days_with_you=days_with_you(), wines_dict=get_excel_wines())
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
