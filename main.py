8bc6ed714dfc8a686ba1a9e80557450f6efd72e3
Maxim-Pekov <max.pekov@gmail.com>
2022-07-30 23:47:28 +0300

    change incorrect name of variable

--- main.py
+++ main.py
@@ -3,21 +3,11 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import collections
-from config import SETTING_PATH
+import os
+from dotenv import load_dotenv
 
 
-def get_excel_wines():
-    '''Возвращает словарь с данными о вине полученным из Excel файла клиента'''
-
-    excel_wines = pandas.read_excel(SETTING_PATH, na_values='some_dummy_na_value', keep_default_na=False)
-    wines = excel_wines.to_dict(orient='records')
-    wine_cluster = collections.defaultdict(list)
-    for wine in wines:
-        wine_cluster[wine['Категория']].append(wine)
-    return wine_cluster
-
-
-def get_right_ending_word_year(company_age):
+def get_correct_form_of_the_word(company_age):
     if company_age % 100 in [11, 12, 13, 14]:
         return 'лет'
     elif company_age % 10 == 1:
@@ -30,21 +20,34 @@ def get_right_ending_word_year(company_age):
 def get_company_age():
     '''Возвращает кол-во лет компании'''
 
-    year_start = 1920
+    year_of_foundation = 1920
     today = datetime.datetime.now()
-    company_age = today.year - year_start
-    word_year_in_ru = get_right_ending_word_year(company_age)
-    return f'{company_age} {word_year_in_ru}'
+    company_age = today.year - year_of_foundation
+    correct_form_of_the_word = get_correct_form_of_the_word(company_age)
+    return f'{company_age} {correct_form_of_the_word}'
 
 
 def main():
+    load_dotenv()
+
+    def get_excel_wines():
+        '''Возвращает словарь с данными о вине полученным из Excel файла клиента'''
+
+        excel_wines = pandas.read_excel(SETTING_PATH, na_values='some_dummy_na_value', keep_default_na=False)
+        wines = excel_wines.to_dict(orient='records')
+        wine_cluster = collections.defaultdict(list)
+        for wine in wines:
+            wine_cluster[wine['Категория']].append(wine)
+        return wine_cluster
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
 
     template = env.get_template('template.html')
-
+    BASE_PATH = 'wines.xlsx'
+    SETTING_PATH = os.getenv('SETTING_PATH', BASE_PATH)
     rendered_page = template.render(company_age=get_company_age(), wine_cluster=get_excel_wines())
 
     with open('index.html', 'w', encoding="utf8") as file:
